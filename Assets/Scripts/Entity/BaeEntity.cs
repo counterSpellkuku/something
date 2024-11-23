cc0a81d0055ced2515f906c85fe4b44a24bf6aaa
COMPY07 <dlwjdgns13579@gmail.com>
2024-11-22 22:35:27 -0800

    fix: BaseEntity Movement

--- Assets/Scripts/Entity/BaeEntity.cs
+++ Assets/Scripts/Entity/BaeEntity.cs
@@ -8,11 +8,19 @@ namespace Entity
     [RequireComponent(typeof(BoxCollider2D))]
     public class BaseEntity : MonoBehaviour
     {
+        [Header("Stats")]
         [SerializeField] public string name;
         [SerializeField] protected float currentHp, maxHp;
         [SerializeField] protected float speed, maxSpeed;
+        
+        [Header("Component")]
         [SerializeField] protected Rigidbody2D rigid;
         [SerializeField] protected BoxCollider2D collider;
+        
+        [Header("RigidBody Settings")]
+        [SerializeField] private float acceleration = 50f;
+        [SerializeField] private float deceleration = 50f;
+        [SerializeField] protected Vector2 currentVelocity;
 
 
         public void Awake()
@@ -46,6 +54,31 @@ namespace Entity
         
         public virtual void Dead() {
             
+        }
+        
+        protected void Move(Vector2 moveInput) {
+            Vector2 targetVelocity = moveInput * speed;
+            
+            if (moveInput != Vector2.zero)
+            {
+                currentVelocity = Vector2.MoveTowards(
+                    currentVelocity,
+                    targetVelocity,
+                    acceleration * Time.fixedDeltaTime
+                );
+            }
+            else
+            {
+                currentVelocity = Vector2.MoveTowards(
+                    currentVelocity,
+                    Vector2.zero,
+                    deceleration * Time.fixedDeltaTime
+                );
+            }
+
+            currentVelocity = Vector2.ClampMagnitude(currentVelocity, maxSpeed);
+
+            rigid.linearVelocity = currentVelocity;
         }
 
 
