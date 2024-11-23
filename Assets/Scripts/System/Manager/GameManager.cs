2e73d27924609a651a908bcf01bb15078ec36366
minyee2913 <leegm0759@gmail.com>
2024-11-23 15:31:26 +0900

    GameManager base

--- Assets/Scripts/System/Manager/GameManager.cs
+++ Assets/Scripts/System/Manager/GameManager.cs
@@ -1,5 +1,60 @@
+using System.Collections;
+using Entity.Player;
 using UnityEngine;
 
-public class GameManager : MonoBehaviour
-{
+namespace System.Manager {
+    public enum GameState {
+        None,
+        Starting,
+        Started,
+        Ended,
+    }
+    public class GameManager : MonoBehaviour
+    {
+        public static GameManager Instance {get; private set;}
+
+        [SerializeField]
+        bool StartGameOnAwake;
+
+        #region Game Control Values
+        public GameState state;
+        public Vector2 startPos;
+        public float playTime;
+        
+        #endregion
+        [SerializeField]
+        PlayerController player;
+        void Awake() {
+            player = FindFirstObjectByType<PlayerController>();
+
+            if (StartGameOnAwake) {
+                StartGame();
+            }
+        }
+
+        public void StartGame() {
+            StartCoroutine(startingGame());
+        }
+
+        IEnumerator startingGame() {
+            //시작 전 초기화
+            state = GameState.Starting;
+            player.transform.position = startPos;
+            playTime = 0;
+
+            yield return new WaitForSeconds(1);
+
+            state = GameState.Started;
+        }
+
+        void Update() {
+            if (state == GameState.Started) {
+                playTime += Time.deltaTime;
+            }
+        }
+
+        void OnDestroy() {
+            Instance = null;
+        }
+    }
 }
