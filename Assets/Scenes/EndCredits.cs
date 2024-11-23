74d66b33455b2808dfb148cc7a0d7128c9a6462e
Chaeho Yu <chaeho1234@naver.com>
2024-11-24 08:16:15 +0900

    Add : end credit

---
+++ Assets/Scenes/EndCredits.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EndCredits : MonoBehaviour
+{
+    public RectTransform contentTransform; // ContentContainer의 RectTransform
+    public float duration;         // 30초 동안 이동
+    private Vector2 startPos;
+    private Vector2 endPos;
+    private float elapsedTime = 0f;
+
+    void Start()
+    {
+        // 초기 시작 위치와 끝 위치 설정
+        startPos = contentTransform.anchoredPosition;
+        endPos = new Vector2(startPos.x, 0f); // posY를 0으로 설정
+    }
+
+    void Update()
+    {
+        if (elapsedTime < duration)
+        {
+            elapsedTime += Time.deltaTime;
+            float t = elapsedTime / duration; // 0에서 1까지의 시간 비율
+            contentTransform.anchoredPosition = Vector2.Lerp(startPos, endPos, t); // 선형 보간
+        }
+    }
+}
