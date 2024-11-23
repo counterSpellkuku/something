5d4ac75a56e52c92f9a7e5545881eee2e99d18e4
COMPY07 <dlwjdgns13579@gmail.com>
2024-11-22 22:02:51 -0800

    add: PlayerController

--- Assets/Scripts/Entity/BaeEntity.cs
+++ Assets/Scripts/Entity/BaeEntity.cs
@@ -1,18 +1,26 @@
+using System;
 using System.Reflection;
 using UnityEngine;
 
 namespace Entity
 {
+    [RequireComponent(typeof(Rigidbody2D))]
+    [RequireComponent(typeof(BoxCollider2D))]
     public class BaseEntity : MonoBehaviour
     {
         [SerializeField] public string name;
         [SerializeField] protected float currentHp, maxHp;
-        [SerializeField] protected float speed;
+        [SerializeField] protected float speed, maxSpeed;
         [SerializeField] protected Rigidbody2D rigid;
         [SerializeField] protected BoxCollider2D collider;
-        
-        
-        
+
+
+        public void Awake()
+        {
+            rigid = GetComponent<Rigidbody2D>();
+            collider = GetComponent<BoxCollider2D>();
+        }
+
         public virtual bool GetDamage(float damage, BaseEntity attacker) {
             // 특수 상황인 경우 override
             bool cancel = false;
--- Assets/Scripts/Entity/Player/PlayerController.cs
+++ Assets/Scripts/Entity/Player/PlayerController.cs
@@ -1,12 +1,69 @@
+using UnityEngine;
+
 namespace Entity.Player
 {
     public class PlayerController : BaseEntity {
         
         
-        public void Start()
-        {
+        [Header("Movement Settings")]
+        [SerializeField] private float acceleration = 50f;
+        [SerializeField] private float deceleration = 50f;
+        
+
+        private Vector2 moveInput;
+        private Vector2 currentVelocity;
+
+        private void Start() {
+            rigid = GetComponent<Rigidbody2D>();
+            rigid.gravityScale = 0f;
+            rigid.constraints = RigidbodyConstraints2D.FreezeRotation;
+        }
+
+        private void Update() {
+            moveInput.x = Input.GetAxisRaw("Horizontal");
+            moveInput.y = Input.GetAxisRaw("Vertical");
+            moveInput = moveInput.normalized;
             
         }
+
+        private void FixedUpdate() {
+            Move();
+        }
+
+        private void Move() {
+            
+            
+            Vector2 targetVelocity = moveInput * speed;
+            Debug.Log(targetVelocity);
+            
+            if (moveInput != Vector2.zero)
+            {
+                currentVelocity = Vector2.MoveTowards(
+                    currentVelocity,
+                    targetVelocity,
+                    acceleration * Time.fixedDeltaTime
+                );
+            }
+            else
+            {
+                currentVelocity = Vector2.MoveTowards(
+                    currentVelocity,
+                    Vector2.zero,
+                    deceleration * Time.fixedDeltaTime
+                );
+            }
+
+            currentVelocity = Vector2.ClampMagnitude(currentVelocity, maxSpeed);
+
+            rigid.linearVelocity = currentVelocity;
+        }
+        
+       
+        
+        
+        
+        
+        
         
         
         
